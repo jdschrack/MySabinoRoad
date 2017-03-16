1f5cd126763b7d7aa0817d4ac0ef253644ffc0e2
Jeremey Schrack <jdschrack@gmail.com>
2017-03-16 09:23:04 -0700

    Create Repo

---
+++ MySabino/Services/RockApi.cs
@@ -0,0 +1,23 @@
+﻿using RestSharp.Portable;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+
+namespace MySabino
+{
+
+	public static class RockApi
+	{
+		const string AuthorizationTokenHeaderKey = "Authorization-Token";
+
+		public static string BaseUrl { get; private set; }
+		static string AuthorizationKey { get; set; }
+		static HttpWebRequest Request { get; set; }
+		static RockApi()
+		{
+			//Request = new HttpWebRequest();
+		}
+	}
+}
---
+++ MySabino/View/MySabinoPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Xamarin.Forms;
+using System;
+
+namespace MySabino
+{
+	public partial class MySabinoPage : ContentPage
+	{
+		public MySabinoPage()
+		{
+			InitializeComponent();
+		}
+
+		void OnButtonClicked(object sender, EventArgs args)
+        {
+			
+        }
+	}
+}
